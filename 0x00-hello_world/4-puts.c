f5b6e0a90b57510541b35164f0caf25e6c2ad1e8
officialpryz <afolabipraise43@gmail.com>
2023-10-05 13:16:14 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- *
  * Return: always 0
  */
 int main(void) {
   /**
- This is the main function that prints a message*/
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+   * This is the main function that prints a message
+   */
+  puts("Programming is like building a multilingual puzzle");
+  return (0);
 }
