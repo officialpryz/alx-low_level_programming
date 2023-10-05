ab0a52eb90c70569bfebca17628c1fb4ab228ccc
officialpryz <afolabipraise43@gmail.com>
2023-10-05 11:08:37 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-  //This is the main function that prints a message.
+  /**
+ This is the main function that prints a message*/
   puts("\"Programming is like building a multilingual puzzle");
   return(0);
 }
