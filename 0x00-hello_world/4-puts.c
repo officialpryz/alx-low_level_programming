2b5d1623b7d568a9c8595d5473cce56b3808f13a
officialpryz <afolabipraise43@gmail.com>
2023-10-05 13:20:28 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,6 +8,6 @@ int main(void) {
   /**
    * This is the main function that prints a message
    */
-  puts("Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
