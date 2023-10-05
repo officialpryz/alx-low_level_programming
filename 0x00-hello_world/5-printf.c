179e46bddd95b7d117062433e0d5f56194b1eb30
officialpryz <afolabipraise43@gmail.com>
2023-10-05 14:00:13 -0700

    use the function printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
 int main(void)
 {
   /* This is the main function that prints a message */
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
