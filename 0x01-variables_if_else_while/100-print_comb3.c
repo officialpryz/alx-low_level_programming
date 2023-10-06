32368f605411dbd3ea9d1d579a372e316b1bd13f
officialpryz <afolabipraise43@gmail.com>
2023-10-06 14:04:10 -0700

    print two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: alway return 0
+ *
+ */
+int main(void)
+{
+  /**
+   * main function
+   *
+   * start here 
+   *
+   */
+int a;
+int b;
+
+for (a = 10; a <= 19; a++)
+{
+for (b = 10; b <= 19; b++)
+{
+if ((b % 10) > (a % 10))
+{
+putchar((a % 10) + '0');
+putchar((b % 10) + '0');
+if (a != 18 || b != 19)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
