088b412479edafdd9ec25daf2e64aef3b65470d3
officialpryz <afolabipraise43@gmail.com>
2023-10-06 09:05:45 -0700

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stduo.h>
+
+/**
+ * main - entry point
+ *
+ * Return: alway returns
+ *
+ */
+int main(void)
+{
+  /**
+   * main function
+   *
+   * start here
+   */
+  int a = 0;
+
+for (; a < 10 ; a++)
+printf("%d", a);
+putchar('\n');
+return (0);
+}
