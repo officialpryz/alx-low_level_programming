4675e09aa3ba5997669c573080ca6339672da270
officialpryz <afolabipraise43@gmail.com>
2023-10-06 12:37:37 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+  /**
+   * main function
+   *
+   * start here
+   *
+   */
+  char a;
+
+for (a = 'z'; a >= 'a'; a--)
+putchar(a);
+putchar('\n');
+return (0);
+}
