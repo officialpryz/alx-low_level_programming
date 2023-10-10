a4768e65321c34987db5556e8552cba20d5d9c40
officialpryz <afolabipraise43@gmail.com>
2023-10-10 08:50:09 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - print alphabets
+ *
+ * return: always 0
+ *
+ */
+void print_alphabet(void)
+{
+  /**
+   * start here
+   *
+   */
+ char x;
+ for (x = 'a'; x <= 'z'; x++)
+ {
+ _putchar(x)
+ }
+ _putchar('\n')
+ }
