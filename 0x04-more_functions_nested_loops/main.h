b616c3418c42c3db013148cb7b9081c66e36646c
officialpryz <afolabipraise43@gmail.com>
2023-10-12 07:14:44 -0700

    3 print

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
+
 int _isupper(int c);
 
 int _isdigit(int c);
@@ -20,5 +22,7 @@ void print_diagonal(int n);
 void print_square(int size);
 
 void print_triangle(int size);
+
+void print_number(int n);
 
 #endif
