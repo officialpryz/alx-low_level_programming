c42a94533be26c9b931e8992ca80f4bf127dae07
officialpryz <afolabipraise43@gmail.com>
2023-10-12 08:15:03 -0700

    6 line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - prints a line
+ *
+ * @x: integer to print stuff?
+ */
+void print_line(int x)
+{
+int a;
+
+for (a = '0'; a < x; a++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
