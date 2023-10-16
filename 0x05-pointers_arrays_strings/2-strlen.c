979ddaf460bf1870266c9f2ece7e019f11f9e458
officialpryz <afolabipraise43@gmail.com>
2023-10-16 14:08:32 -0700

    function that returns the length of a string.
    git returns the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,15 +2,15 @@
 /**
  * _strlen - gets character string 
  *
- *@s: character to getstring
- *Return: returns length
+ * @s: character to getstring
+ * Return: returns length
  *
  */
 int _strlen(char *s)
 {
 int i;
 
-for (i = 0; s[i] !='\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {}
 return (i);
 }
