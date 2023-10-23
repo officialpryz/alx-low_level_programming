062cfc63f806d178c72c611c6942949b835385b8
officialpryz <afolabipraise43@gmail.com>
2023-10-23 11:02:35 -0700

    fucntion that print the sum of two diagonals of a matrix of integers

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,7 +13,7 @@ int diagT = 0;
 
 for (i = 0; i < size; i++)
 {
-diagO += a[(i * size) + i ];
+diagO += a[(i * size) + i];
 diagT += a[(size - 1) + ((size - 1) * i)];
 }
 printf("%d, %d\n", diagO, diagT);
